61e77fddeaf7f5f1323bd2c5cd9524c8df75fc9c
vratika06khandelwal <vratikakhandelwal44611@gmail.com>
2022-01-11 18:11:46 +0530

    updated

--- SpringBootJDBC/src/main/java/com/example/demo/SpringBootJdbcApplication.java
+++ SpringBootJDBC/src/main/java/com/example/demo/SpringBootJdbcApplication.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootJdbcApplication {
 
 	public static void main(String[] args) {
+	
 		SpringApplication.run(SpringBootJdbcApplication.class, args);
+		
 	}
 
 }
